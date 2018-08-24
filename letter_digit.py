4dfa2d84fb2c2293be690b1e72ee73b0afb1867e
farid7666 <42679603+farid7666@users.noreply.github.com>
2018-08-24 17:46:08 -0500

    Update letter_digit.py

--- letter_digit.py
+++ letter_digit.py
@@ -4,13 +4,16 @@ Created on Fri Aug 24 16:50:28 2018
 
 @author: farid
 """
-
+#input sentence
 sentence = input("write down the sentence: ")
+#length of the sentence
 length = len(sentence)
 
+#counter for digit and letter
 digit = 0
 letter = 0
 
+#loop for counting digits and letters
 for i in range(length):
     temp=sentence[i]
     if temp.isdigit():
@@ -18,6 +21,7 @@ for i in range(length):
     if temp.isalpha():
         letter +=1
 
+#printing number of letters and digits
 print ("Number of digits in the sentence:", digit)
 print ("Number of the letters in the sentence", letter)
 
