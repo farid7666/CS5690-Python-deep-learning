22d9faa71db5403325647b11589fcf493c472aee
Farid Uddin Ahmed <fanqb@mail.umkc.edu>
2018-09-14 17:56:54 -0500

    ICP 4

--- ICP 4/numpy.py
+++ ICP 4/numpy.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 14 16:31:32 2018
 
-@author: farid
-"""
-
+#import numpy modules
 import numpy as np
+#creating a array having random variables from 1 to 20 in size 15
 a=np.random.randint(20,size=(15))
+#Counting the number of time each element present in the array
 counts = np.bincount(a)
+#Print the array
 print (a)
+#Print the element which present maximum times in the array
 print (np.argmax(counts))
