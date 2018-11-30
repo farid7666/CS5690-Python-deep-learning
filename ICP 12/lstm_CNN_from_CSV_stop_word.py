d05cbdcd8231ebfdee337b35dced0bf04947e6d6
Farid Uddin Ahmed <fanqb@mail.umkc.edu>
2018-11-30 17:34:14 -0600

    Module 2 ICP6

---
+++ ICP 12/lstm_CNN_from_CSV_stop_word.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+
+# Embedding
+max_features = 20000
+maxlen = 100
+embedding_size = 128
+
+# Convolution
+kernel_size = 5
+filters = 64
+pool_size = 4
+
+# LSTM
+lstm_output_size = 70
+
+# Training
+batch_size = 30
+epochs = 2
+
+'''
+Note:
+batch_size is highly sensitive.
+Only 2 epochs are needed as the dataset is very small.
+'''
+df1 = pd.read_csv('./imdb_master.csv',encoding='latin-1', nrows=50000)
+df1['label'] = df1['label'].map({'neg':0,'pos':1})
+df1['review'] = [w for w in df1['review'] if not w in stopwords.words('english')]
+tokenizer = Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(df1['review'])
+list_tokenized_train= tokenizer.texts_to_sequences(df1['review'])
+X= pad_sequences(list_tokenized_train,maxlen=maxlen)
+y= df1['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(X_test, y_test))
+score, acc = model.evaluate(X_test, y_test, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
